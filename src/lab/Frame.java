66fd3427e89a26089d5237c32467acb3b1d28ce1
Peter Aoun <keifa.peter@gmail.com>
2017-05-14 17:22:57 +0200

    Implementation of user story 2

--- src/lab/Frame.java
+++ src/lab/Frame.java
@@ -4,9 +4,11 @@ public class Frame {
 	
 	public int firstThrow = 0;
 	public int secondThrow = 0;
+	public int score = 0;
 	
 	Frame(int firstThrow, int secondThrow) {
 		this.firstThrow = firstThrow;
 		this.secondThrow = secondThrow;
+		this.score = this.firstThrow + this.secondThrow;
 	}
 }
