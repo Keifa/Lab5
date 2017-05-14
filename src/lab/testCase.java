d33d57a61252e520f987dcac77abb6597b6e939c
Peter Aoun <keifa.peter@gmail.com>
2017-05-14 18:06:35 +0200

    Test case for user story 5

--- src/lab/testCase.java
+++ src/lab/testCase.java
@@ -79,4 +79,20 @@ public class testCase {
 		g.addFrame(2, 6);
 		assertEquals(81, g.gameScore);
 	}
+	
+	@Test
+	public void userStory5() {
+		Game g = new Game();
+		g.addFrame(10, 0);
+		g.addFrame(3, 6);
+		g.addFrame(7, 2);
+		g.addFrame(3, 6);
+		g.addFrame(4, 4);
+		g.addFrame(5, 3);
+		g.addFrame(3, 3);
+		g.addFrame(4, 5);
+		g.addFrame(8, 1);
+		g.addFrame(2, 6);
+		assertEquals(94, g.gameScore);
+	}
 }
